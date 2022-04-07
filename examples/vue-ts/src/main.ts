16f496940ddf23d9ea47b14545a7573171d4ca8a
codebender828 <jbakebwa@163.com>
2022-04-07 19:42:37 +0800

    feat: update docs

--- examples/vue-ts/src/main.ts
+++ examples/vue-ts/src/main.ts
@@ -3,11 +3,10 @@ import App from './App.vue';
 import { createApp } from 'vue';
 import Chakra from '@chakra-ui/vue-next';
 import { chakra, domElements } from '@chakra-ui/vue-system';
-import { ViteSSG } from 'vite-ssg';
 // @ts-ignore
 import generatedRoutes from 'virtual:generated-pages';
 import { setupLayouts } from 'virtual:generated-layouts';
-import { hydrate } from '@emotion/css';
+import { createHead } from '@vueuse/head';
 
 import * as icons from './utils/icons';
 import theme from './theme/extend';
@@ -16,25 +15,20 @@ import { AxiaSolana } from './solana.plugin';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = setupLayouts(generatedRoutes);
+const head = createHead();
 
 const router = createRouter({
   routes,
   history: createWebHistory(),
 });
 const app = createApp(App);
+
 app.use(router);
+app.use(head);
 
 app.use(AxiaSolana, {
   network: SolanaNetworks.devnet,
 });
-
-app.use(Chakra, {
-  extendTheme: theme,
-  icons: {
-    library: icons,
-  },
-});
-
 domElements.forEach((tag) => {
   app.component(`chakra.${tag}`, chakra(tag));
 });
