fa674d6e73bcc4409c768ff2ae9d1ae52180e04e
codebender828 <jbakebwa@163.com>
2023-03-20 17:36:33 +0800

    chore: create changeset

---
+++ .changeset/fresh-phones-judge.md
@@ -0,0 +1,8 @@
+---
+'@usemirrorworld/mirage.core': patch
+'@usemirrorworld/mirage.vanilla': patch
+'@usemirrorworld/mirage.vue': patch
+'@usemirrorworld/mirage.utils': patch
+---
+
+Hotfix for namespace
--- packages/frameworks/vue/src/index.ts
+++ packages/frameworks/vue/src/index.ts
@@ -1,4 +1,4 @@
-import { AUCTION_HOUSE_PROGRAM_ID } from '@mirrorworld/mirage.core';
+import { AUCTION_HOUSE_PROGRAM_ID } from '@usemirrorworld/mirage.core';
 
 console.log({
   TODO: 'Create hook for this',
